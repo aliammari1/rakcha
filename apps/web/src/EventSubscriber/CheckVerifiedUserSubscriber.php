5830e7b8c546d14bc861f2c63f58f0d615931538
Ali Ammari <ammari.ali.0001@gmail.com>
2026-07-11 20:50:45 +0000

    refactor: update method signatures to include return types and clean up configuration files

--- apps/web/src/EventSubscriber/CheckVerifiedUserSubscriber.php
+++ apps/web/src/EventSubscriber/CheckVerifiedUserSubscriber.php
@@ -21,7 +21,7 @@ class CheckVerifiedUserSubscriber implements EventSubscriberInterface
         $this->router = $router;
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             CheckPassportEvent::class => ['onCheckPassport', -10],
